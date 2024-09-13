8acbd6bdf85b23d88b4341e7b9b98deee19e810a
Nabin Shrestha <nabins724@gmail.com>
2024-09-13 20:28:13 +1000

    Resolved overflowing boxes and alignment issues

--- script.js
+++ script.js
@@ -3,62 +3,66 @@
 const container = document.querySelector(".container");
 
 // Saving number entered by user to variable "number".
-let number = (prompt("Please Enter the number of boxer you want between 1 and 100: ", 0));
-
+let userInput = (prompt("Please Enter the number of boxes you want in a row between numbers 1 and 100: ", 0));
 
 // Using for loop to create a grid based on the number entered by user.
-for (let i = 0; i < number * number; i++) {
-    // creating div element and assigning it to variable newElement.
-    let newElement = document.createElement("div");
+for (let i = 0; i < userInput; i++) {
+    for (let j = 0; j < userInput; j++) {
+        // creating div element and assigning it to variable newElement.
+        let newElement = document.createElement("div");
 
-    // Setting style to the newly created div based on user input.
-    let boxWidth = 500 / number;
-    let boxHeight = 500 / number;
-    newElement.setAttribute("style", `display: flex; width: ${boxWidth}px; height: ${boxHeight}px;`);
-    console.log(i);
+        // Adding newly created div elements to the container class.
+        newElement.classList.add("newElement");
+        container.appendChild(newElement);
 
-    // Adding newly created div elements to the container class.
-    container.appendChild(newElement);
+        // Setting style to the newly created div based on user input.
+        let boxWidth = 500 / userInput;
+        let boxHeight = 500 / userInput;
+        newElement.setAttribute("style", `width: ${boxWidth}px; height: ${boxHeight}px;`);
 
-    // Changing background color of the square divs randomly using mouseleave event.
-    newElement.addEventListener("mouseenter", () => {
-        newElement.style.backgroundColor = "pink";
-    });
-    newElement.addEventListener("mouseleave", () => {
-        let randomNumber = Math.floor(Math.random()*10);
-
-        if(randomNumber === 0){
-            newElement.style.backgroundColor = "red";
-        }
-        else if(randomNumber === 1){
-            newElement.style.backgroundColor = "green";
-        }
-        else if(randomNumber === 2){
-            newElement.style.backgroundColor = "purple";
-        }
-        else if(randomNumber === 3){
-            newElement.style.backgroundColor = "orange";
-        }
-        else if(randomNumber === 4){
-            newElement.style.backgroundColor = "yellow";
-        }
-        else if(randomNumber === 5){
-            newElement.style.backgroundColor = "brown";
-        }
-        else if(randomNumber === 6){
-            newElement.style.backgroundColor = "black";
-        }
-        else if(randomNumber === 7){
-            newElement.style.backgroundColor = "white";
-        }
-        else if(randomNumber === 8){
-            newElement.style.backgroundColor = "light-blue";
-        }
-        else if(randomNumber === 9){
+        // Changing background color of the square divs randomly using mouseleave event.
+        newElement.addEventListener("mouseenter", () => {
             newElement.style.backgroundColor = "pink";
-        }
-        else if(randomNumber === 10){
-            newElement.style.backgroundColor = "grey";
-        }
-    });
+        });
+        newElement.addEventListener("mouseleave", () => {
+            let randomNumber = Math.floor(Math.random() * 10);
+
+            // I used if else statement instead of switch case, will it have effect on efficiency of program??
+
+            if (randomNumber === 0) {
+                newElement.style.backgroundColor = "red";
+            }
+            else if (randomNumber === 1) {
+                newElement.style.backgroundColor = "green";
+            }
+            else if (randomNumber === 2) {
+                newElement.style.backgroundColor = "purple";
+            }
+            else if (randomNumber === 3) {
+                newElement.style.backgroundColor = "orange";
+            }
+            else if (randomNumber === 4) {
+                newElement.style.backgroundColor = "yellow";
+            }
+            else if (randomNumber === 5) {
+                newElement.style.backgroundColor = "brown";
+            }
+            else if (randomNumber === 6) {
+                newElement.style.backgroundColor = "black";
+            }
+            else if (randomNumber === 7) {
+                newElement.style.backgroundColor = "white";
+            }
+            else if (randomNumber === 8) {
+                newElement.style.backgroundColor = "light-blue";
+            }
+            else if (randomNumber === 9) {
+                newElement.style.backgroundColor = "pink";
+            }
+            else if (randomNumber === 10) {
+                newElement.style.backgroundColor = "grey";
+            }
+        });
+    }
+
 }
